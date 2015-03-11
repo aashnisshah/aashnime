c1b6d0a83ec80051eb65cf1579163428fba45a3f
Aashni Shah <contact@aashni.me>
2015-03-11 02:09:49 -0400

    updating footer links to actually link to my social media

--- wordpress/footer.php
+++ wordpress/footer.php
@@ -6,14 +6,12 @@
 
                 <span class="pull-right">
                     <ul class="social-media">
-                        <li><a href="" alt="Git Hub"><img src="http://aashni.me/images/somacro/border/github.png" width="32px" height="32px"></a></li>
-                        <li><a href="" alt="Facebook"><img src="http://aashni.me/images/somacro/border/facebook.png" width="32px" height="32px"></a></li>
-                        <li><a href="" alt="Linked In"><img src="http://aashni.me/images/somacro/border/linkedin.png" width="32px" height="32px"></a></li>
-                        <li><a href="" alt="Twitter"><img src="http://aashni.me/images/somacro/border/twitter.png" width="32px" height="32px"></a></li>
-                        <li><a href="" alt="Instagram"><img src="http://aashni.me/images/somacro/border/instagram.png" width="32px" height="32px"></a></li>
-                        <li><a href="" alt="Stack Overflow"><img src="http://aashni.me/images/somacro/border/stackoverflow.png" width="32px" height="32px"></a></li>
-                        <li><a href="" alt="Google Plus"><img src="http://aashni.me/images/somacro/border/googleplus.png" width="32px" height="32px"></a></li>
-                        <li><a href="" alt="Picture Me Not"><img src="http://aashni.me/images/somacro/border/picturemenot.png" width="32px" height="32px"></a></li>
+                        <li><a href="http://ca.linkedin.com/in/aashnisshah" alt="Linked In"><img src="http://aashni.me/images/somacro/border/linkedin.png" width="32px" height="32px"></a></li>
+                        <li><a href="http://github.com/aashnisshah" alt="Git Hub"><img src="http://aashni.me/images/somacro/border/github.png" width="32px" height="32px"></a></li> 
+                        <li><a href="http://www.twitter.com/aashnisshah" alt="Twitter"><img src="http://aashni.me/images/somacro/border/twitter.png" width="32px" height="32px"></a></li>
+                        <li><a href="http://instagram.com/aashnisshah" alt="Instagram"><img src="http://aashni.me/images/somacro/border/instagram.png" width="32px" height="32px"></a></li>
+                        <li><a href="http://stackoverflow.com/users/1989265/aashnisshah" alt="Stack Overflow"><img src="http://aashni.me/images/somacro/border/stackoverflow.png" width="32px" height="32px"></a></li>
+                        <li><a href="http://picturemenot.com" alt="Picture Me Not"><img src="http://aashni.me/images/somacro/border/picturemenot.png" width="32px" height="32px"></a></li>
                     </ul>
                 </span>
             </footer>
