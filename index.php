68f4f256c0697407b3ecf1741681b88cc18d2cdb
Aashni Shah <contact@aashni.me>
2015-03-11 02:16:33 -0400

    adding coming soon links to index page content

--- index.php
+++ index.php
@@ -73,9 +73,9 @@
                     <h1>Aashni Shah</h1>
                     <!-- <p class="lead">Always doing what she enjoys...</p> -->
 
-                    <p class="lead">I was born and raised in <a href="">Nairobi</a>, studied in <a href="">Toronto</a>, and have worked in <a href="">Jakarta</a>, <a href="">Nairobi</a>, <a href="">Seattle</a> and <a href="">Toronto</a>.</p>
+                    <p class="lead">I was born and raised in <a href="http://aashni.me/coming.php">Nairobi</a>, studied in <a href="http://aashni.me/coming.php">Toronto</a>, and have worked in <a href="http://aashni.me/coming.php">Jakarta</a>, <a href="http://aashni.me/coming.php">Nairobi</a>, <a href="http://aashni.me/coming.php">Seattle</a> and <a href="http://aashni.me/coming.php">Toronto</a>.</p>
 
-                    <p class="lead">I enjoy <a href="">planning</a> events, and have recently started <a href="">speaking</a> at events as well. I love <a href="">hackathons</a>, coding my own personal <a href="">mini projects</a> and writing <a href="">blog posts</a>.
+                    <p class="lead">I enjoy <a href="http://aashni.me/coming.php">planning</a> events, and have recently started <a href="http://aashni.me/coming.php">speaking</a> at events as well. I love <a href="http://aashni.me/coming.php">hackathons</a>, coding my own personal <a href="http://aashni.me/coming.php">mini projects</a> and writing <a href="http://blog.aashni.me">blog posts</a>.
 
                     <span class="pull-right">
                     <ul class="social-media">
