a4d2e427c9d499dc31f67d2dfb1bcdbe76e37720
Aashni Shah <contact@aashni.me>
2015-04-03 21:33:36 -0400

    my about me page

---
+++ about.php
@@ -0,0 +1,65 @@
+<?php include 'header.php'; ?>
+
+	<!-- Page Content -->
+    <div class="container jumbotron vertical-center transparent">
+
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <div class="heading-section inner cover">
+                    <h1>About Aashni</h1>
+                    <!-- <p class="lead">Always doing what she enjoys...</p> -->
+
+                    <p class="lead">
+                    	My name is Aashni Shah, and I am currently on my PEY Internship at <a href="http://www.oanda.com" target="_blank">OANDA</a>, a Forex Company in Downtown Toronto. I shall be returning for my final year of study at the <a href="http://www.utoronto.ca" target="_blank">University of Toronto</a> in September, where I study <a href="http://web.cs.toronto.edu/" target="_blank">Computer Science</a>.
+                    </p>
+
+                    <p class="lead">
+                    	I use this website and my <a href="http://blog.aashni.me" target="_blank">blog</a> to share my experiences, whether it's hacking at a hackathon, or speaking at a conference. I will occasionally include other things that I find interesting, such as my side projects, tech-y events or cool coding tips.
+                    </p>
+
+                    <p class="lead">
+                    	I have previously worked at LivEpicly in <a href="http://aashni.me/coming.php">Jakarta</a>, Kingsway Tyres in <a href="http://aashni.me/coming.php">Nairobi</a>, Amazon in <a href="http://aashni.me/coming.php">Seattle</a> and OANDA in <a href="http://aashni.me/coming.php">Toronto</a>.
+                    </p>
+
+
+                	<p class="lead">
+                		To know more about my experiences, read my <a href="http://aashni.me/downloads/resume.pdf" target="_blank">resume</a>
+                	</p>
+
+                    <p class="lead">
+                    	You can find me on the following social media sites:
+
+                    	<span class="pull-right">
+	                    	<ul class="social-media">
+	                        	<li>
+	                        		<a href="http://ca.linkedin.com/in/aashnisshah" alt="Linked In"><img src="http://aashni.me/images/somacro/border/linkedin.png" width="32px" height="32px"></a>
+	                        	</li>
+	                        	<li>
+	                        		<a href="http://github.com/aashnisshah" alt="Git Hub"><img src="http://aashni.me/images/somacro/border/github.png" width="32px" height="32px"></a>
+	                        	</li> 
+	                        	<li>
+	                        		<a href="http://www.twitter.com/aashnisshah" alt="Twitter"><img src="http://aashni.me/images/somacro/border/twitter.png" width="32px" height="32px"></a>
+	                        	</li>
+	                        	<li>
+	                        		<a href="http://instagram.com/aashnisshah" alt="Instagram"><img src="http://aashni.me/images/somacro/border/instagram.png" width="32px" height="32px"></a>
+	                        	</li>
+	                        	<li>
+	                        		<a href="http://stackoverflow.com/users/1989265/aashnisshah" alt="Stack Overflow"><img src="http://aashni.me/images/somacro/border/stackoverflow.png" width="32px" height="32px"></a></li>
+	                        	<li>
+	                        		<a href="http://picturemenot.com" alt="Picture Me Not"><img src="http://aashni.me/images/somacro/border/picturemenot.png" width="32px" height="32px"></a>
+	                        	</li>
+	                    	</ul>
+                		</span>
+                	</p>
+
+
+
+                </div>
+            </div>
+        </div>
+        <!-- /.row -->
+
+    </div>
+    <!-- /.container -->
+
+<?php include 'footer.php'; ?>
