c270f08cf30509cacb544f3bdef2c64de9e9850f
Aashni Shah <contact@aashni.me>
2015-04-03 21:33:25 -0400

    separating pages so that we can use php includes

--- index.php
+++ index.php
@@ -1,69 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>Aashni Shah</title>
-    <link rel="shortcut icon" type="image/x-icon" href="http://aashni.me/favicon.ico" />
-
-    <!-- Bootstrap Core CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/style.css" rel="stylesheet">
-
-    <!-- Custom CSS -->
-    <style>
-    body {
-        padding-top: 70px;
-        /* Required padding for .navbar-fixed-top. Remove if using .navbar-static-top. Change if height of navigation changes. */
-    }
-    </style>
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
-</head>
-
-<body>
-
-    <!-- Navigation -->
-    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-        <div class="container">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-          <span class="sr-only">Toggle navigation</span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </button>
-      </div>
-
-      <!-- Collect the nav links, forms, and other content for toggling -->
-      <div class="collapse navbar-collapse navbar-ex1-collapse">
-        <ul class="nav navbar-nav">
-          <li><a href="http://aashni.me">Aashni</a></li>
-          <li><a href="http://aashni.me/coming.php">About Me</a></li>
-          <li><a href="http://blog.aashni.me">Blogger</a></li>
-          <li><a href="http://aashni.me/coming.php">Planner</a></li>
-          <li><a href="http://aashni.me/coming.php">Hacker</a></li>
-          <li><a href="http://aashni.me/coming.php">Speaker</a></li>
-          <li><a href="http://aashni.me/coming.php">Contact Me</a></li>
-        </ul>
-      </div><!-- /.navbar-collapse -->
-        </div>
-        <!-- /.container -->
-    </nav>
-    
+<?php include 'header.php'; ?>
 
     <!-- Page Content -->
     <div class="container jumbotron vertical-center transparent">
@@ -79,15 +14,15 @@
                     <p class="lead">I enjoy <a href="http://aashni.me/coming.php">planning</a> events, and have recently started <a href="http://aashni.me/coming.php">speaking</a> at events as well. I love <a href="http://aashni.me/coming.php">hackathons</a>, coding my own personal <a href="http://aashni.me/coming.php">mini projects</a> and writing <a href="http://blog.aashni.me">blog posts</a>.
 
                     <span class="pull-right">
-                    <ul class="social-media">
-                        <li><a href="http://ca.linkedin.com/in/aashnisshah" alt="Linked In"><img src="http://aashni.me/images/somacro/border/linkedin.png" width="32px" height="32px"></a></li>
-                        <li><a href="http://github.com/aashnisshah" alt="Git Hub"><img src="http://aashni.me/images/somacro/border/github.png" width="32px" height="32px"></a></li> 
-                        <li><a href="http://www.twitter.com/aashnisshah" alt="Twitter"><img src="http://aashni.me/images/somacro/border/twitter.png" width="32px" height="32px"></a></li>
-                        <li><a href="http://instagram.com/aashnisshah" alt="Instagram"><img src="http://aashni.me/images/somacro/border/instagram.png" width="32px" height="32px"></a></li>
-                        <li><a href="http://stackoverflow.com/users/1989265/aashnisshah" alt="Stack Overflow"><img src="http://aashni.me/images/somacro/border/stackoverflow.png" width="32px" height="32px"></a></li>
-                        <li><a href="http://picturemenot.com" alt="Picture Me Not"><img src="http://aashni.me/images/somacro/border/picturemenot.png" width="32px" height="32px"></a></li>
-                    </ul>
-                </span>
+                        <ul class="social-media">
+                            <li><a href="http://ca.linkedin.com/in/aashnisshah" alt="Linked In"><img src="http://aashni.me/images/somacro/border/linkedin.png" width="32px" height="32px"></a></li>
+                            <li><a href="http://github.com/aashnisshah" alt="Git Hub"><img src="http://aashni.me/images/somacro/border/github.png" width="32px" height="32px"></a></li> 
+                            <li><a href="http://www.twitter.com/aashnisshah" alt="Twitter"><img src="http://aashni.me/images/somacro/border/twitter.png" width="32px" height="32px"></a></li>
+                            <li><a href="http://instagram.com/aashnisshah" alt="Instagram"><img src="http://aashni.me/images/somacro/border/instagram.png" width="32px" height="32px"></a></li>
+                            <li><a href="http://stackoverflow.com/users/1989265/aashnisshah" alt="Stack Overflow"><img src="http://aashni.me/images/somacro/border/stackoverflow.png" width="32px" height="32px"></a></li>
+                            <li><a href="http://picturemenot.com" alt="Picture Me Not"><img src="http://aashni.me/images/somacro/border/picturemenot.png" width="32px" height="32px"></a></li>
+                        </ul>
+                    </span>
 
                 </div>
             </div>
@@ -97,12 +32,5 @@
     </div>
     <!-- /.container -->
 
-    <!-- jQuery Version 1.11.1 -->
-    <script src="js/jquery.js"></script>
 
-    <!-- Bootstrap Core JavaScript -->
-    <script src="js/bootstrap.min.js"></script>
-
-</body>
-
-</html>
+<?php include 'footer.php'; ?>
