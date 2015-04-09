07db935fc92addb777874b614ac3bae30ec45713
Aashni Shah <contact@aashni.me>
2015-04-08 21:21:15 -0400

    splitting page into includable parts, adding about me info

---
+++ navigation.php
@@ -0,0 +1,28 @@
+<!-- Navigation -->
+<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+    <div class="container">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+      <span class="sr-only">Toggle navigation</span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+    </button>
+  </div>
+
+  <!-- Collect the nav links, forms, and other content for toggling -->
+  <div class="collapse navbar-collapse navbar-ex1-collapse">
+    <ul class="nav navbar-nav">
+      <li><a href="http://aashni.me">Aashni</a></li>
+      <li><a href="http://aashni.me/about.php">About Me</a></li>
+      <li><a href="http://blog.aashni.me">Blogger</a></li>
+      <li><a href="http://aashni.me/coming.php">Planner</a></li>
+      <li><a href="http://aashni.me/coming.php">Hacker</a></li>
+      <li><a href="http://aashni.me/coming.php">Speaker</a></li>
+      <li><a href="http://aashni.me/coming.php">Contact Me</a></li>
+    </ul>
+  </div><!-- /.navbar-collapse -->
+    </div>
+    <!-- /.container -->
+</nav>
---
+++ planner.php
@@ -0,0 +1,23 @@
+<?php include 'header.php'; ?>
+
+    <!-- Page Content -->
+    <div class="container jumbotron vertical-center transparent">
+
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <div class="heading-section inner cover">
+                    <h1>Planning Events</h1>
+                    <!-- <p class="lead">Always doing what she enjoys...</p> -->
+
+                    <p class="lead">Planning Events is so much fun!</p>
+
+                </div>
+            </div>
+        </div>
+        <!-- /.row -->
+
+    </div>
+    <!-- /.container -->
+
+
+<?php include 'footer.php'; ?>
