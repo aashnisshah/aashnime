dbc046e36e2210166b36bdf15076c99ba609e9ba
Aashni Shah <aashnisshah@Aashnis-MacBook-Pro.local>
2015-10-13 02:14:31 -0500

    updated navigation?

--- navigation.php
+++ navigation.php
@@ -15,10 +15,8 @@
   <div class="collapse navbar-collapse navbar-ex1-collapse">
     <ul class="nav navbar-nav">
       <li><a href="http://aashni.me">Aashni</a></li>
-      <li><a href="http://aashni.me/about.php">About Me</a></li>
+      <li><a href="http://aashni.me/events.php">Events</a></li>
       <li><a href="http://blog.aashni.me">Blog</a></li>
-      <li><a href="http://aashni.me/coming.php">Events + Hackathons</a></li>
-      <li><a href="http://aashni.me/coming.php">Speaker</a></li>
       <li><a href="http://aashni.me/downloads/resume.pdf">Resume</a></li>
       <li><a href="http://aashni.me/coming.php">Contact Me</a></li>
     </ul>
