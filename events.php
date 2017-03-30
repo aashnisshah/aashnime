8bf2fc4b613f96f83f0aa076c50e0261c316362c
Aashni Shah <contact@aashni.me>
2017-03-29 22:06:49 -0700

    updated events page and fixed(?) styling

---
+++ events.php
@@ -0,0 +1,25 @@
+<?php include 'header.php'; ?>
+
+    <div class="col-lg-12 col-lg-offset-2">
+
+        <h1>Events</h1>
+
+        <p class="lead">I enjoy organizing, speaking at and attending numerous different type of tech events. Here is a brief overview of some of them.</p>
+
+        <h2>Events I've Organized</h2>
+        <p class="lead"><a href="http://www.uofthacks.com" target="_blank">UofTHacks III</a></p>
+        <p class="lead"><a href="http://www.cutc.ca" target="_blank">Canadian Undergraduate Tech Conference</a> - <a href="http://blog.aashni.me/2015/05/canadian-undergraduate-tech-conference/" target="_blank">blog post</a></p>
+        <p class="lead"><a href="http://www.uofthacks.com" target="_blank">UofTHacks II</a></p>
+        <p class="lead"><a href="http://www.cssu.ca" target="_blank">Computer Science Student Union President</a></p>
+        <p class="lead"><a href="http://www.uofthacks.com" target="_blank">UofTHacks I</a> - <a href="http://blog.aashni.me/2013/12/uofthacks-2013-an-overview/" target="_blank">blog post</a></p>
+
+        <h2>Events I've Spoken At</h2>
+        <p class="lead"><a href="http://www.cutc.ca" target="_blank">Canadian Undergraduate Tech Conference</a> - <a href="http://blog.aashni.me/2015/05/canadian-undergraduate-tech-conference/" target="_blank">blog post</a></p>
+        <p class="lead"><a href="http://fitc.ca/article/fitc-womens-make-and-mix-2015/" target="_blank">FITC Women Make and Mix</a> - <a href="http://blog.aashni.me/2015/04/fitc-womens-mix-and-make-social/" target="_blank">blog post</a></p>
+        <p class="lead"><a href="http://www.oanda.com" target="_blank">OANDA Lunch and Learn</a></p>
+        <p class="lead"><a href="http://2015.podcamptoronto.com/sessions/civic-engagement-technology-the-perfect-marriage/" target="_blank">PodCamp Toronto 2015</a> - <a href="http://blog.aashni.me/2015/03/podcamp-toronto/" target="_blank">blog post</a></p>
+
+        <h2>Event's I've Participated In</h2>
+        <p class="lead"><a href="http://medicine.utoronto.ca/news/aspiring-entrepreneurs-face-u-t-dragons-den-health-care" target="_blank">Health EDGE</a> - <a href="http://web.cs.toronto.edu/news/current/Project_Nash_by_Computer_Science_Students_awarded__5K_HealthEDGE_fellowship.htm?DateTime=635955390000000000&PageMode=View" target="_blank">Fellowship Winner with Project Nash</a></p>
+
+<?php include 'footer.php'; ?>
