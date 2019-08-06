2fb3622b6ae4d53aaaf4271f27141f995f8df22e
Aashni Shah <contact@aashni.me>
2019-08-05 20:14:25 -0400

    Restructure files and add gatsby project as main codebase

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+import HomePageNavigation from "./HomePageNavigation"
+import SocialLink from "./SocialLink"
+import SocialLinks from "./SocialLinks"
+export { HomePageNavigation, SocialLink, SocialLinks }
