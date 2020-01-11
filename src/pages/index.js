3b35e42ccdebf627b488f3b49ad0f1357899a5b3
Aashni Shah <contact@aashni.me>
2020-01-12 01:13:02 +0300

    Made blog index point to blog, added generic index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,83 +1,35 @@
 import React from "react"
 import { Link, graphql } from "gatsby"
 
-import Bio from "../components/bio"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import { rhythm } from "../utils/typography"
-import blogDate from "../utils/dates"
 
-class BlogIndex extends React.Component {
+class IndexPage extends React.Component {
   render() {
     const { data } = this.props
     const siteTitle = data.site.siteMetadata.title
-    const posts = data.allMarkdownRemark.edges
 
     return (
       <Layout location={this.props.location} title={siteTitle}>
-        <SEO title="All posts" />
-        <Bio />
-        {posts.map(({ node }) => {
-          const title = node.frontmatter.title || node.fields.slug
-          return (
-            <article key={node.fields.slug}>
-              <header>
-                <h3
-                  style={{
-                    marginBottom: rhythm(1 / 4),
-                  }}
-                >
-                  <Link
-                    style={{ boxShadow: `none` }}
-                    to={
-                      "blog/" +
-                      blogDate(node.frontmatter.date) +
-                      "/" +
-                      node.fields.slug
-                    }
-                  >
-                    {title}
-                  </Link>
-                </h3>
-                <small>{node.frontmatter.date}</small>
-              </header>
-              <section>
-                <p
-                  dangerouslySetInnerHTML={{
-                    __html: node.frontmatter.description || node.excerpt,
-                  }}
-                />
-              </section>
-            </article>
-          )
-        })}
+        <SEO title="Aashni" />
+        <h1>Aashni Shah</h1>
+        <p>I make things, and I do things.</p>
+
+        <Link style={{ boxShadow: `none` }} to={"blog/"}>
+          Blog
+        </Link>
       </Layout>
     )
   }
 }
 
-export default BlogIndex
+export default IndexPage
 
 export const pageQuery = graphql`
   query {
     site {
       siteMetadata {
         title
-      }
-    }
-    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
-      edges {
-        node {
-          excerpt
-          fields {
-            slug
-          }
-          frontmatter {
-            date
-            title
-            description
-          }
-        }
       }
     }
   }
