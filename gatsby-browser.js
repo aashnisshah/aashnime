f0a635085e7231d16bbd69c12d7344b003679133
Aashni <contact@aashni.me>
2020-04-19 01:51:10 +0300

    adding primsjs styling for code blocks

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,3 +3,6 @@ import "typeface-montserrat"
 import "typeface-merriweather"
 
 // import "prismjs/themes/prism.css"
+require("./src/styles/prism-theme.css")
+require("prismjs/plugins/line-numbers/prism-line-numbers.css")
+require("prismjs/plugins/command-line/prism-command-line.css")
