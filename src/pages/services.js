e7ecbd586eca17a38aef877dfbefb37ce4929d82
Aashni <contact@aashni.me>
2020-05-13 16:49:04 +0300

    Updating services

--- src/pages/services.js
+++ src/pages/services.js
@@ -13,47 +13,47 @@ class ServicesPage extends React.Component {
       <Layout location={this.props.location} title={siteTitle}>
         <SEO title="Services | Aashni" />
         <h1>Services</h1>
-        <h2>Coaching</h2>
+        <h2>Career Coaching</h2>
         <p>
-          Job hunting is scary, whether you're looking for an internship, your
-          first job after college, or you're seasoned but looking for the next
-          great thing. I'd love to help you gain confidence, prep and ace the
-          interview. I've given over 100 interviews in the last 2 years, and
-          have given plenty of talks on prepping for the job interview process.
+          Whether you're looking for an internship, your first job after
+          college, or you're a seasoned professional looking for your next gig,
+          job hunting is painful, scary and exhausting. I've conducted over 100
+          interviews in the last 2 years, as well as given talks on how to prep
+          for the job hunt. Don't struggle alone - schedule an exploratory call
+          now!
+          <ul>
+            <li>build up your resume and portfolio</li>
+            <li>how to find jobs and apply to them</li>
+            <li>practice makes perfect, so we'll do some mock interviews</li>
+            <li>
+              access to resources I've used for years to land interviews and get
+              job offers
+            </li>
+            <li>lot's of hype and support</li>
+          </ul>
         </p>
 
-        <p>
-          We'll cover everything from building up your resume and portfolio, to
-          finding and applying for jobs, and practicing with mock interviews.
-          I'll share some of the resources I've been using to land interviews
-          for years, and hype you up before any interview.
-        </p>
-
-        <p>
-          Sounds like what you need? Schedule an{" "}
+        <h3>
           <a
             href="https://calendly.com/aashnisshah/connect"
             target="_blank"
             rel="noopener noreferrer"
           >
-            introductory call
-          </a>{" "}
-          now!
+            Schedule an introductory call now!
+          </a>
+        </h3>
+
+        <h2>Freelance Work</h2>
+        <p>
+          Working on an interesting project and need an extra set of hands? I
+          have hands on experience with Software Engineering and Product
+          Management. With my experience leading Elixir Labs, I have experience
+          solving engineering and product problems, validating product ideas,
+          hiring a team and making team synergies work better. Take the next
+          steps to grow your business and set up an exploratory call now!
         </p>
 
-        <h2>Consulting</h2>
-        <p>
-          Are you building a startup and trying to figure out where and how to
-          get started technically? I've built many products and projects
-          end-to-end and would love to share my knowledge.
-        </p>
-
-        <p>
-          Our sessions can cover basic ideation and validation techniques to
-          deeper technical dives to understand how to build your idea.
-        </p>
-
-        <p>
+        <h3>
           Interested?
           <a
             href="https://calendly.com/aashnisshah/connect"
@@ -64,7 +64,7 @@ class ServicesPage extends React.Component {
             Let's connect
           </a>
           !
-        </p>
+        </h3>
       </Layout>
     )
   }
