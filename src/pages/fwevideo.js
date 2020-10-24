d326a303030699668404885e7bb225e1d5f16606
Aashni <contact@aashni.me>
2020-10-23 20:37:53 -0400

    actually redirect to FWE video

---
+++ src/pages/fwevideo.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+if (typeof window !== `undefined`)
+  window.location.replace(`https://www.youtube.com/watch?v=x9JGvgrxqhE`)
+
+const FweVideo = () => {
+  return (
+    <>
+      Click to go to{" "}
+      <a href="https://www.youtube.com/watch?v=x9JGvgrxqhE">video</a>
+    </>
+  )
+}
+
+export default FweVideo
